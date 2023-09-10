5631c2fc0607f5120810236f57626b69463ce2fc
javier <javiercyu@gmail.com>
2023-09-10 14:04:45 -0700

    switch to nerfstudio CLI

--- nerfbridge/method_configs.py
+++ nerfbridge/method_configs.py
@@ -2,14 +2,10 @@
 # https://github.com/nerfstudio-project/nerfstudio/blob/df784e96e7979aaa4320284c087d7036dce67c28/nerfstudio/configs/method_configs.py
 
 """
-Method configurations
+NerfBridge Method Configs
 """
 
 from __future__ import annotations
-
-from typing import Dict
-
-import tyro
 
 from nerfstudio.cameras.camera_optimizers import CameraOptimizerConfig
 from nerfstudio.configs.base_config import ViewerConfig
@@ -21,55 +17,41 @@ from nerfbridge.ros_datamanager import ROSDataManagerConfig
 from nerfbridge.ros_dataparser import ROSDataParserConfig
 from nerfbridge.ros_trainer import ROSTrainerConfig
 
-method_configs: Dict[str, ROSTrainerConfig] = {}
-descriptions = {
-    "ros_nerfacto": "Run the nerfstudio nerfacto method on data streamed from ROS.",
-}
+from nerfstudio.plugins.types import MethodSpecification
 
-method_configs["ros_nerfacto"] = ROSTrainerConfig(
-    method_name="ros_nerfacto",
-    steps_per_eval_batch=500,
-    steps_per_save=30000,
-    max_num_iterations=30000,
-    mixed_precision=True,
-    pipeline=VanillaPipelineConfig(
-        datamanager=ROSDataManagerConfig(
-            dataparser=ROSDataParserConfig(
-                aabb_scale=0.8,
+RosNerfacto = MethodSpecification(
+    config=ROSTrainerConfig(
+        method_name="ros-nerfacto",
+        steps_per_eval_batch=500,
+        steps_per_save=30000,
+        max_num_iterations=30000,
+        mixed_precision=True,
+        pipeline=VanillaPipelineConfig(
+            datamanager=ROSDataManagerConfig(
+                dataparser=ROSDataParserConfig(
+                    aabb_scale=0.8,
+                ),
+                train_num_rays_per_batch=4096,
+                eval_num_rays_per_batch=4096,
+                camera_optimizer=CameraOptimizerConfig(
+                    mode="SO3xR3",
+                    optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2),
+                ),
             ),
-            train_num_rays_per_batch=4096,
-            eval_num_rays_per_batch=4096,
-            camera_optimizer=CameraOptimizerConfig(
-                mode="SO3xR3",
-                optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2),
-            ),
+            model=NerfactoModelConfig(eval_num_rays_per_chunk=1 << 15),
         ),
-        model=NerfactoModelConfig(eval_num_rays_per_chunk=1 << 15),
+        optimizers={
+            "proposal_networks": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": None,
+            },
+            "fields": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": None,
+            },
+        },
+        viewer=ViewerConfig(num_rays_per_chunk=20000),
+        vis="viewer",
     ),
-    optimizers={
-        "proposal_networks": {
-            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-            "scheduler": None,
-        },
-        "fields": {
-            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-            "scheduler": None,
-        },
-    },
-    viewer=ViewerConfig(num_rays_per_chunk=20000),
-    vis="viewer",
+    description="Run NerfBridge with the Nerfacto model, and train with streamed RGB images.",
 )
-
-
-AnnotatedBaseConfigUnion = (
-    tyro.conf.SuppressFixed[  # Don't show unparseable (fixed) arguments in helptext.
-        tyro.conf.FlagConversionOff[
-            tyro.extras.subcommand_type_from_defaults(
-                defaults=method_configs, descriptions=descriptions
-            )
-        ]
-    ]
-)
-"""Union[] type over config types, annotated with default instances for use with
-tyro.cli(). Allows the user to pick between one of several base configurations, and
-then override values in it."""
